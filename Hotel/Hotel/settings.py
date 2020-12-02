8774bf25e7452064a8140c849b6ea10144fd0ba9
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2020-12-02 16:42:13 +0530

    login and register settings

--- Hotel/Hotel/settings.py
+++ Hotel/Hotel/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'hotelbooking',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost'
     }
 }
 
--- Hotel/login/views.py
+++ Hotel/login/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 # Create your views here.
 def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            print("Success")
+        else:
+            messages.error(request,"Not found in database")
+            return render(request,'app/login.html')
     return render(request,'app/login.html')
+def register(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['name']
+        email=request.POST['email']
+        password1=request.POST['password']
+        password2=request.POST['re_password']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already exists')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email is already exists')
+            else:
+                messages.error(request,'User Created')
+                user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save()
+                #return render(request,'app/register.html',{'messages':messages})
+
+        else:
+            messages.error(request, 'Password must be same')
+        return render(request,'app/register.html')
+    else:
+        return render(request,'app/register.html')
