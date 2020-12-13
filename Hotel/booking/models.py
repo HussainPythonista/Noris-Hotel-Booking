368bfbb637f520e9c92122d7ddcb749ae051ac07
HussainPythonista <60617460+HussainPythonista@users.noreply.github.com>
2020-12-13 15:42:48 +0530

    change something

--- Hotel/booking/models.py
+++ Hotel/booking/models.py
@@ -9,11 +9,9 @@ from django.conf import settings # For access the data which stored in auth_user
 class Booking(models.Model):
     """Django data model Booking"""
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-
     room=models.ForeignKey(Room,on_delete=models.CASCADE)
-    Check_in = models.DateTimeField(datetime.datetime.strptime(str(datetime.datetime.now()),'%Y-%m-%d %H:%M:%S.%f').strftime("%Y-%m-%d %H:%M"))
-    Check_out = models.DateTimeField(datetime.datetime.strptime(str(datetime.datetime.now()),'%Y-%m-%d %H:%M:%S.%f').strftime("%Y-%m-%d %H:%M"))
- 
+    Check_in = models.DateTimeField()
+    Check_out = models.DateTimeField()
     class Meta:
         verbose_name = 'Booking'
         verbose_name_plural = 'Bookings'
--- Hotel/booking/views.py
+++ Hotel/booking/views.py
@@ -6,6 +6,7 @@ from mainapp.models import Room_Type,Room
 from django.shortcuts import render
 from .bookingFunction import avalablity
 from .models import Booking
+from django.utils.timezone import make_aware
 
 # Create your views here.
 
@@ -20,31 +21,28 @@ def booking(request,room):
         get_roomType=Room_Type.objects.get(roomtype=request.POST['type'])
         roomid=get_roomType.id
         getout=request.POST['check_out']  
-        check_out=datetime.datetime.strptime(getout,'%m/%d/%Y %H:%M %p').strftime('%Y-%m-%d %H:%M:%S+00:00')
+        check_out=datetime.datetime.strptime(getout,'%m/%d/%Y %H:%M %p').strftime('%Y-%m-%d %H:%M:%S')
         getin=request.POST['check_in']  
-        check_in=datetime.datetime.strptime(getin,'%m/%d/%Y %H:%M %p').strftime('%Y-%m-%d %H:%M:%S+00:00')
-        check_in=datetime.datetime.strptime(check_in,'%Y-%m-%d %H:%M:%S+00:00')
-        check_out=(datetime.datetime.strptime(check_out,'%Y-%m-%d %H:%M:%S+00:00'))
-        print(type(check_out))
+        check_in=datetime.datetime.strptime(getin,'%m/%d/%Y %H:%M %p').strftime('%Y-%m-%d %H:%M:%S')
+        check_in=make_aware(datetime.datetime.strptime(check_in,'%Y-%m-%d %H:%M:%S'))
+        check_out=make_aware(datetime.datetime.strptime(check_out,'%Y-%m-%d %H:%M:%S'))
+    
     #This can set the values id to roomtype id
-        room_list=Room.objects.filter(room_type_id=get_roomType)
-        user_book=request.user
+        room_list=Room.objects.filter(room_type=roomid)
         avalible_rooms=[]
         for room in room_list:
             if avalablity.check_avaliblity(room,check_in,check_out):
-                avalible_rooms.append(room)
+                
                 if len(avalible_rooms)>0:
-                    room=avalible_rooms
                     book_room=Booking.objects.create(
-                        user=user_book,
+                        user=request.user,
                         room=room,
                         Check_in=check_in,
                         Check_out=check_out
                     )
                     book_room.save()
                     return HttpResponse(book_room)
-            else:
-                return HttpResponse("Not found")
+            
     type_of_room=Room_Type.objects.get(roomtype=room)
     context={'type':type_of_room}
     return render(request,'app/book.html',context)
